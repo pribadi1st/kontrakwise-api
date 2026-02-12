a60f5ad2620531f292c6975e4f6d401937ef02a4
pribadi1st <pribadiridwan@rocketmail.com>
2026-02-12 19:44:46 +0100

    Feat: add cors

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 from app.api.main import api_router
 from app.core.config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title= settings.PROJECT_NAME, description= settings.PROJECT_DESCRIPTION, version= settings.PROJECT_VERSION)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(api_router)
 
 @app.get("/", tags=["root"])
